36e9c6666e5d44cc03be9650cbf2d2cfcb9815c8
0x9000 <tuningtristan@gmail.com>
2019-09-01 06:38:43 -0400

    test commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello")
+const discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login("NjE3MTE1NDM5NDQ4OTgxNTE1.XWmbrA.SFGNXrudykqWj8yNIeTR8CwXkD0");
+
+client.on("ready", () =>{
+  console.log("the bot is logged in");
+});
+
+  client.on("message", message => {
+    if(message.author.bot)
+    return;
+
+    if(message.content.toLowerCase{} === "hello")
+    message.channel.send("hi");
+  });
